c158adf2ba3197a4fd13d06c399eaec1b7d1ac84
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-03-03 11:20:48 +0530

    Update index.js

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -2,15 +2,17 @@ import {Link} from 'react-router-dom'
 import './index.css'
 
 const TeamCard = props => {
-  const {details} = props
-  const {id, name, teamImageUrl} = details
+  const {teamDetails} = props
+  const {name, id, teamImageURL} = teamDetails
+
   return (
-    <Link to={`/team-matches/${id}`}>
-      <li className="teams-container">
-        <img src={teamImageUrl} alt={name} className="team-logo" />
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link-item">
+        <img src={teamImageURL} alt={name} className="team-logo" />
         <p className="team-name">{name}</p>
-      </li>
-    </Link>
+      </Link>
+    </li>
   )
 }
+
 export default TeamCard
